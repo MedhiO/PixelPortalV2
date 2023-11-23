85add372772871e5d0b0844f94bc49cad7f5b104
Medhi OUKILI <medhioukili@outlook.fr>
2023-11-23 11:13:29 +0100

    maj includes

---
+++ includes/entete.php
@@ -0,0 +1,7 @@
+<a href="../index.html">
+            <div class="entete" id="entete">
+                <img src="../images/portal1.png" id="portal1" class="img-fluid" alt="portal orange">
+                <h1>PIXELPORTAL</h1>
+                <img src="../images/portal2.png" id="portal2" class="img-fluid" alt="portal blue">
+            </div>
+        </a>
---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+<footer>
+        <div class="nousSuivre" id="nousSuivre">NOUS SUIVRE</div>
+        <div id="logoReseaux" class="logoReseaux">
+            <img src="../images/facebook.png" id="facebook" class="img-fluid" alt="logo facebook">
+            <img src="../images/marque-x.png" id="X" class="img-fluid" alt="logo X">
+            <img src="../images/instagram.png" id="instagram" class="img-fluid" alt="logo instagram">
+            <img src="../images/youtube.png" id="youtube" class="img-fluid" alt="logo youtube">
+        </div>
+        <div id="mention" class="mention">Contact | L'équipe | Informations légales | C.G.U | C.G.V | Modération | Politique de confidentialité | Cookies | Newsletter</div>
+</footer>
