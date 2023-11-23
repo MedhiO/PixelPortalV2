6028421142ac7fec3dd77afc5938ed32a0e39b26
Medhi OUKILI <medhioukili@outlook.fr>
2023-11-23 11:55:38 +0100

    maj index

--- index.php
+++ index.php
@@ -25,7 +25,13 @@
 <body>
     <!-- Défini le contenu du header -->
     <header>
-        <?php require_once "../includes/entete.php"?>
+        <a href="./index.php">
+            <div class="entete" id="entete">
+                <img src="../images/portal1.png" id="portal1" class="img-fluid" alt="portal orange">
+                <h1>PIXELPORTAL</h1>
+                <img src="../images/portal2.png" id="portal2" class="img-fluid" alt="portal blue">
+            </div>
+        </a>
         <div class="descriptif" id="descriptif">Accueil</div>
     </header>   
     <!-- Défini le contenu du main -->
@@ -33,14 +39,14 @@
         <div class="boutonAccueil" id="boutonAccueil">
             <div class="row">
                 <div class="col"></div>
-                <div class="col"><a href="./html/register.html"><input type="submit" id="boutonRegister" class="boutonRegister" value="S'ENREGISTRER"></a></div>
+                <div class="col"><a href="./views/register.php"><input type="submit" id="boutonRegister" class="boutonRegister" value="S'ENREGISTRER"></a></div>
                 <div class="col"></div>
                 <div class="col"></div>
                 <div class="col"></div>
             </div>
             <div class="row">
                 <div class="col"></div>
-                <div class="col"><a href="./html/login.html"><input type="submit" id="boutonLogin" class="boutonLogin" value="   CONNEXION   "></a></div>
+                <div class="col"><a href="./views/login.php"><input type="submit" id="boutonLogin" class="boutonLogin" value="   CONNEXION   "></a></div>
                 <div class="col"></div>
                 <div class="col"></div>
                 <div class="col"></div>
@@ -48,7 +54,7 @@
         </div>
     </main>
     <!-- Défini le contenu du footer -->
-    <?php require_once "../includes/footer.php"?>
+    <?php require_once "./includes/footer.php"?>
     <!-- Défini le chemin vers le script bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
