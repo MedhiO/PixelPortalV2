34f49250d4b6db5e5ac5bd1cfe80a79aa33d98cb
Medhi OUKILI <medhioukili@outlook.fr>
2023-11-23 14:58:16 +0100

    maj includes

--- includes/connexiondb.php
+++ includes/connexiondb.php
@@ -12,15 +12,13 @@ $bdd="pixelportal";
 
 try{
 //variable de connection à la bdd
-$con = new
-PDO('mysql:host='.$serveur.';dbname='.$bdd,$user,$password);
-$con->setAttribute(PDO::ATTR_ERRMODE,
-PDO::ERRMODE_EXCEPTION);
+$con = new PDO('mysql:host='.$serveur.';dbname='.$bdd,$user,$password);
+$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 return $con;
 }
 //gestion des erreurs
 catch(PDOException $error){
-echo 'N°'.$error->getCode().'<br/>';
-die ('Erreur : '.$error->getMessage().'<br/>');
+    echo 'N°'.$error->getCode().'<br/>';
+    die ('Erreur : '.$error->getMessage().'<br/>');
 }
 }
