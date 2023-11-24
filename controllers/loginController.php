7e208af53afdca6350f0a3cd494107389fa2edc8
Medhi OUKILI <medhioukili@outlook.fr>
2023-11-24 15:40:34 +0100

    maj controller

--- controllers/loginController.php
+++ controllers/loginController.php
@@ -6,10 +6,6 @@ $con = connectdb();
 $email2 = $_POST['email2'];
 $pwd2 = $_POST['password2'];
 
-// Affichage du contenu des variables pour le débogage
-echo "Contenu de \$email2 : " . $email2 . "<br>";
-echo "Contenu de \$pwd2 : " . $pwd2 . "<br>";
-
 // Vérifie si les champs sont remplis
 if (empty($email2) || empty($pwd2)) {
     echo "Veuillez remplir tous les champs.";
@@ -24,21 +20,7 @@ if (empty($email2) || empty($pwd2)) {
 
     $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    // Affichage du contenu de $row pour le débogage
-    echo "Contenu de \$row : ";
-    print_r($row);
-    echo "<br>";
-
-    echo "Email saisi : " . $email2 . "<br>";
-    echo "Mot de passe saisi : " . $pwd2 . "<br>";
-    echo "Email stocké : " . $row["emailUtilisateur"] . "<br>";
-    echo "Mot de passe stocké : " . $row["motDePasseUtilisateur"] . "<br>";
-
-
     if ($row) {
-        // Affichage du mot de passe haché pour le débogage
-        echo "Mot de passe haché stocké : " . $row["motDePasseUtilisateur"] . "<br>";
-
         // Vérifie si les informations saisies correspondent aux informations stockées
         if ($email2 != $row["emailUtilisateur"] || !password_verify($pwd2, $row["motDePasseUtilisateur"])) {
             echo "Les informations saisies sont incorrectes.";
